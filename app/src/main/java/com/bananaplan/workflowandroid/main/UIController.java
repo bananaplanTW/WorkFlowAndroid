cafac62dd7ddd89f06216b1f516d79ad19157775
logicmelody <logicmelody@gmail.com>
2015-05-30 17:14:02 +0800

    Insert DrawerMenu and JobAssign page

--- app/src/main/java/com/bananaplan/workflowandroid/main/UIController.java
+++ app/src/main/java/com/bananaplan/workflowandroid/main/UIController.java
@@ -1,5 +1,7 @@
 package com.bananaplan.workflowandroid.main;
 
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
@@ -9,6 +11,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 
 import com.bananaplan.workflowandroid.R;
+import com.bananaplan.workflowandroid.jobassign.JobAssignFragment;
 
 
 /**
@@ -26,6 +29,15 @@ public class UIController {
     private DrawerLayout mDrawerLayout;
     private ActionBarDrawerToggle mDrawerToggle;
 
+    private FragmentManager mFragmentManager;
+    private DrawerMenuFragment mDrawerMenuFragment;
+    private JobAssignFragment mJobAssignFragment;
+
+
+    private static class FragmentTag {
+        public static final String TAG_DRAWER_MENU_FRAGMENT = "tag_drawer_menu_fragment";
+        public static final String TAG_JOB_ASSIGN_FRAGMENT = "tag_job_assign_fragment";
+    }
 
     public UIController(ActionBarActivity activity) {
         mMainActivity = activity;
@@ -36,9 +48,11 @@ public class UIController {
     }
 
     private void initialize() {
+        mFragmentManager = mMainActivity.getFragmentManager();
         findViews();
         initActionbar();
         initDrawer();
+        initFragments();
     }
 
     private void findViews() {
@@ -67,6 +81,24 @@ public class UIController {
         };
         mDrawerToggle.syncState();
         mDrawerLayout.setDrawerListener(mDrawerToggle);
+    }
+
+    private void initFragments() {
+        FragmentTransaction fragmentTransaction = mFragmentManager.beginTransaction();
+
+        mDrawerMenuFragment = (DrawerMenuFragment) mFragmentManager.findFragmentByTag(FragmentTag.TAG_DRAWER_MENU_FRAGMENT);
+        if (mDrawerMenuFragment == null) {
+            mDrawerMenuFragment = new DrawerMenuFragment();
+        }
+        fragmentTransaction.add(R.id.drawer_menu_container, mDrawerMenuFragment);
+
+        mJobAssignFragment = (JobAssignFragment) mFragmentManager.findFragmentByTag(FragmentTag.TAG_JOB_ASSIGN_FRAGMENT);
+        if (mJobAssignFragment == null) {
+            mJobAssignFragment = new JobAssignFragment();
+        }
+        fragmentTransaction.add(R.id.content_container, mJobAssignFragment);
+
+        fragmentTransaction.commit();
     }
 
 }
