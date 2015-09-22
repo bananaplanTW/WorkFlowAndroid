e9fc19437a82dbfc0a21dbd71752008a13512da6
logicmelody <logicmelody@gmail.com>
2015-09-22 10:26:50 +0800

    [Preload] Add PreloadActivity
    1. Add PreloadActivity to load working data from server before displaying MainActivity
    2. RestfulAPI for connecting with server

---
+++ app/src/main/java/com/bananaplan/workflowandroid/main/PreloadActivity.java
@@ -0,0 +1,96 @@
+package com.bananaplan.workflowandroid.main;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+
+import com.bananaplan.workflowandroid.R;
+import com.bananaplan.workflowandroid.utility.server.RestfulUtils;
+
+import java.util.concurrent.ExecutionException;
+
+
+// TODO: Load data needs to be put in a service
+public class PreloadActivity extends AppCompatActivity {
+
+    private static final String TAG = "PreloadActivity";
+
+    private static final int PRELOAD_TIME_OUT = 500;
+
+    private static final class WorkingDataUrl {
+        public static final String WORKER = "http://10.1.1.41:3000/api/employees";
+    }
+
+    private Handler mHandler;
+    private Runnable mLaunchMainActivity = new Runnable() {
+        @Override
+        public void run() {
+            startActivity(new Intent(PreloadActivity.this, MainActivity.class));
+            Log.d(TAG, "Start MainActivity");
+            finish();
+            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_preload);
+        initialize();
+    }
+
+    private void initialize() {
+        mHandler = new Handler();
+        long startLoading = 0L;
+        long finishLoading = 0L;
+
+        setupActionBar();
+
+        startLoading = System.currentTimeMillis();
+        loadWorkingData();
+        finishLoading = System.currentTimeMillis();
+
+        launchMainActivity(finishLoading - startLoading);
+    }
+
+    private void setupActionBar() {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.tool_bar);
+        setSupportActionBar(toolbar);
+        if (getSupportActionBar() == null) return;
+
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "removeCallbacks");
+        mHandler.removeCallbacks(mLaunchMainActivity);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        launchMainActivity(0);
+    }
+
+    private void loadWorkingData() {
+        try {
+            String weatherJSONString =
+                    new RestfulUtils.GetRequest().execute(WorkingDataUrl.WORKER).get();
+            Log.d(TAG, weatherJSONString);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void launchMainActivity(long timeOut) {
+        timeOut = timeOut > PRELOAD_TIME_OUT ? 0 : PRELOAD_TIME_OUT - timeOut;
+        mHandler.postDelayed(mLaunchMainActivity, timeOut);
+    }
+}
---
+++ app/src/main/java/com/bananaplan/workflowandroid/utility/server/RestfulUtils.java
@@ -0,0 +1,202 @@
+package com.bananaplan.workflowandroid.utility.server;
+
+import android.os.AsyncTask;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+
+
+public class RestfulUtils {
+
+    private static final String TAG = "RestfulUtils";
+
+    public static class GetRequest extends AsyncTask<String, Integer, String> {
+
+        @Override
+        protected String doInBackground(String... urlStrings) {
+            InputStream inputStream = null;
+            String result = null;
+            if (urlStrings[0] != null) {
+                try {
+                    URL url = new URL(urlStrings[0]);
+                    HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                    conn.setReadTimeout(3000);
+                    conn.setConnectTimeout(5000);
+                    conn.setRequestMethod("GET");
+                    conn.setDoInput(true);
+
+                    conn.connect();
+                    int responseCode = conn.getResponseCode();
+                    Log.d("Restful api", "Response Code is : " + responseCode);
+                    inputStream = conn.getInputStream();
+
+                    result = getStringFromInputStream(inputStream);
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                } catch (ProtocolException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return result;
+        }
+    }
+
+    public static class PostRequest extends AsyncTask<String, Integer, String> {
+
+        private String body;
+        public PostRequest (String b) {
+            body = b;
+        }
+        @Override
+        protected String doInBackground(String... urlStrings) {
+            OutputStream outputStream = null;
+            InputStream inputStream = null;
+            String result = null;
+            HttpURLConnection conn = null;
+            if (urlStrings[0] != null) {
+                try {
+                    URL url = new URL(urlStrings[0]);
+                    conn = (HttpURLConnection) url.openConnection();
+                    conn.setRequestProperty("Content-Type", "application/json");
+                    conn.setReadTimeout(15000);
+                    conn.setConnectTimeout(15000);
+                    conn.setDoInput(true);
+                    conn.setDoOutput(true);
+
+                    Log.d("Restful api", "Calling: " + urlStrings[0]);
+                    outputStream = conn.getOutputStream();
+                    outputStream.write(body.getBytes("UTF-8"));
+
+                    conn.connect();
+                    int responseCode = conn.getResponseCode();
+                    Log.d("Restful api", "Response Code is : " + responseCode);
+                    inputStream = conn.getInputStream();
+
+                    result = getStringFromInputStream(inputStream);
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                } catch (ProtocolException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } finally {
+                    conn.disconnect();
+                }
+            }
+            return result;
+        }
+    }
+
+
+    //extract response content
+//    public static String getStatusCode(HttpResponse response){
+//        if(response == null)
+//            return null;
+//        StringBuilder sb = new StringBuilder();
+//        String line = "";
+//        try{
+//            BufferedReader rd = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
+//            while ((line = rd.readLine()) != null) {
+//                sb.append(line);
+//            }
+//        } catch (IOException io){
+//
+//        }
+//        return sb.toString();
+//    }
+
+//    public static HttpResponse restfulPostRequest(String endpoint, List<NameValuePair> pairs) {
+//        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+//        StrictMode.setThreadPolicy(policy);
+//        final HttpParams httpParams = new BasicHttpParams();
+//        HttpConnectionParams.setConnectionTimeout(httpParams, 10000);
+//        HttpConnectionParams.setSoTimeout(httpParams, 3000);
+//        HttpClient client = new DefaultHttpClient(httpParams);
+//        HttpPost post;
+//        if (BuildConfig.DEBUG) {
+//            post = new HttpPost(Constant.AWS_HOST_DEBUG + endpoint);
+//        } else {
+//            post = new HttpPost(Constant.AWS_HOST + endpoint);
+//        }
+//
+//
+//        HttpResponse response = null;
+//        try {
+//            post.setEntity(new UrlEncodedFormEntity(pairs));
+//            response = client.execute(post);
+//
+//        } catch (ConnectTimeoutException cte) {
+//            Log.e("connection timeout ", "time out !!!!");
+//        } catch (UnsupportedEncodingException uee) {
+//
+//        } catch (ClientProtocolException cpe) {
+//            //ignore this exception for now
+//        } catch (IOException ioe) {
+//            //ignore this exception for now
+//        }
+//        return response;
+//    }
+
+    public static InputStream restfulGetRequest (String urlString) {
+        InputStream inputStream;
+
+        try {
+            URL url = new URL(urlString);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setReadTimeout(3000);
+            conn.setConnectTimeout(5000);
+            conn.setRequestMethod("GET");
+            conn.setDoInput(true);
+
+            conn.connect();
+            int responseCode = conn.getResponseCode();
+            Log.d("Restful api", "Response Code is : " + responseCode);
+            inputStream = conn.getInputStream();
+            return inputStream;
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (ProtocolException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static String getStringFromInputStream(InputStream in) {
+        BufferedReader br = null;
+        StringBuilder sb = new StringBuilder();
+        try {
+            String s;
+            br = new BufferedReader(new InputStreamReader(in, "UTF-8"));
+            while ( (s = br.readLine()) != null) {
+                sb.append(s);
+            }
+        } catch (UnsupportedEncodingException e) {
+            Log.d("Error","Unsupport UTF-8 data type");
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (br != null) {
+                try {
+                    br.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return sb.toString();
+    }
+}
