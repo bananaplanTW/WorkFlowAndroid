00827a967789ee1846927fdfd1aa39f354292508
logicmelody <logicmelody@gmail.com>
2015-10-29 14:33:18 +0800

    [DetailedWarning] Information part

--- app/src/main/java/com/bananaplan/workflowandroid/detail/DetailedWarningActivity.java
+++ app/src/main/java/com/bananaplan/workflowandroid/detail/DetailedWarningActivity.java
@@ -6,10 +6,14 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 import com.bananaplan.workflowandroid.R;
+import com.bananaplan.workflowandroid.data.Manager;
+import com.bananaplan.workflowandroid.data.Task;
 import com.bananaplan.workflowandroid.data.TaskWarning;
 import com.bananaplan.workflowandroid.data.WorkingData;
+import com.bananaplan.workflowandroid.utility.Utils;
 
 
 public class DetailedWarningActivity extends AppCompatActivity {
@@ -21,6 +25,14 @@ public class DetailedWarningActivity extends AppCompatActivity {
     private static final String TAG_DETAILED_WARNING_STATUS_FRAGMENT = "tag_detailed_task_status_fragment";
 
     private ActionBar mActionBar;
+
+    private TextView mActionBarWarningName;
+    private TextView mActionBarTaskName;
+
+    private TextView mInformationWarningName;
+    private TextView mInformationTaskName;
+    private TextView mInformationManagerName;
+    private TextView mInformationWarningSpentTime;
 
     private TaskWarning mTaskWarning;
 
@@ -40,7 +52,12 @@ public class DetailedWarningActivity extends AppCompatActivity {
     }
 
     private void findViews() {
-
+        mActionBarWarningName = (TextView) findViewById(R.id.detailed_actionbar_warning_name);
+        mActionBarTaskName = (TextView) findViewById(R.id.detailed_actionbar_warning_task_name);
+        mInformationWarningName = (TextView) findViewById(R.id.detailed_information_warning_name);
+        mInformationTaskName = (TextView) findViewById(R.id.detailed_information_task_name);
+        mInformationManagerName = (TextView) findViewById(R.id.detailed_information_manager_name);
+        mInformationWarningSpentTime = (TextView) findViewById(R.id.detailed_information_warning_spent_time);
     }
 
     private void setupActionBar() {
@@ -52,13 +69,31 @@ public class DetailedWarningActivity extends AppCompatActivity {
             mActionBar.setDisplayShowTitleEnabled(false);
             mActionBar.setDisplayHomeAsUpEnabled(true);
         }
+
+        switch (mTaskWarning.status) {
+            case OPENED:
+                toolbar.setBackgroundColor(getResources().getColor(R.color.warning_opened_background_color));
+                break;
+
+            case CLOSED:
+                toolbar.setBackgroundColor(getResources().getColor(R.color.warning_closed_background_color));
+                break;
+        }
     }
 
     private void setupViews() {
+        Task warningTask = WorkingData.getInstance(this).getTaskById(mTaskWarning.taskId);
+        Manager picManager = WorkingData.getInstance(this).getManagerById(mTaskWarning.managerId);
 
+        mActionBarWarningName.setText(mTaskWarning.name);
+        mActionBarTaskName.setText(warningTask.name);
+        mInformationWarningName.setText(mTaskWarning.name);
+        mInformationTaskName.setText(warningTask.name);
+        mInformationManagerName.setText(picManager.name);
+        mInformationWarningSpentTime.setText(Utils.millisecondsToTimeString(mTaskWarning.spentTime));
     }
 
-    private void setupTaskLog() {
+    private void setupWarningLog() {
 //        FragmentManager fragmentManager = getSupportFragmentManager();
 //        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
 //
